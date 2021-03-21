59a89618d7f6001e54c914f189aff4095b84c922
Patrick <patrickgcauley@gmail.com>
2021-03-20 23:16:47 -0400

    e

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,2 +1,23 @@
 import 'reflect-metadata';
+import express from 'express';
+import { buildSchema } from 'type-graphql';
 import { createConnection } from 'typeorm';
+import { UserResolver } from './resolvers/user';
+import { ApolloServer } from 'apollo-server-express';
+
+(async () => {
+	const app = express();
+
+	await createConnection();
+
+	const apolloServer = new ApolloServer({
+		schema: await buildSchema({
+			resolvers: [UserResolver],
+			validate: false,
+		}),
+	});
+	apolloServer.applyMiddleware({ app });
+	app.listen(4000, () => {
+		console.log('express server started 🥊');
+	});
+})();
